53fa52693b6c6d8723346e9789e54683fd09704d
Kartik Vashishta <kartikv776@gmail.com>
2020-11-10 19:54:16 +0400

    created user and cart sagas for sign in, sign up and sign in with Google and further, converted a few components via styled components

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -2,29 +2,28 @@ import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
-	const priceForStripe = price * 100;
-	const publishableKey =
-		'pk_test_51HUE9RJ4zFBBdBn56FMVzCQM5JC0BDn8bPvMIYM1saeKiKoN6Ez6rMrpAmEXSW8fLBolJ8suLkKHk1NJSOmcdFBs00fslJXHtG';
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
 
-	const onToken = token => {
-		console.log(token);
-		alert('Payment Succesful!');
-	};
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
 
-	return (
-		<StripeCheckout
-			label='Pay Now &#128179;'
-			name='CRWN Clothing Ltd.'
-			billingAddress
-			shippingAddress
-			image='https://svgshare.com/i/CUz.svg'
-			description={`Your total is $${price}`}
-			amount={priceForStripe}
-			panelLabel='Pay Now'
-			token={onToken}
-			stripeKey={publishableKey}
-		/>
-	);
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
 };
 
 export default StripeCheckoutButton;
