cbb37cba2d248319c0292522efc2b5d72301f2f6
Kartik Vashishta <kartikv776@gmail.com>
2020-11-20 08:16:50 +0400

    Adding performance optimizations to codebase with react lazy, suspense and memo

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,11 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Spinner from '../../components/spinner/spinner.component';
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
-import CollectionPageContainer from '../collection/collection.container';
+const CollectionsOverviewContainer = lazy(() =>
+	import('../../components/collections-overview/collections-overview.container')
+);
+const CollectionPageContainer = lazy(() =>
+	import('../collection/collection.container')
+);
 
 const ShopPage = ({ fetchCollectionsStart, match }) => {
 	useEffect(
@@ -17,15 +22,14 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
 
 	return (
 		<div className='shop-page'>
-			<Route
-				exact
-				path={`${match.path}`}
-				component={CollectionsOverviewContainer}
-			/>
-			<Route
-				path={`${match.path}/:collectionId`}
-				component={CollectionPageContainer}
-			/>
+			<Suspense fallback={<Spinner />}>
+				<Route
+					exact
+					path={`${match.path}`}
+					component={CollectionsOverviewContainer}
+				/>
+				{newFunction(match)}
+			</Suspense>
 		</div>
 	);
 };
@@ -35,3 +39,9 @@ const mapDispatchToProps = dispatch => ({
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
+function newFunction(match) {
+	return <Route
+		path={`${match.path}/:collectionId`}
+		component={CollectionPageContainer} />;
+}
+
